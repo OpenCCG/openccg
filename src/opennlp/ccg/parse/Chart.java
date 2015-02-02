8c773de35854e3756b362b5f1c3cdd78e39406ad
Daniel Couto Vale <danielvale@uni-bremen.de>
2015-02-02 13:00:16 +0100

    Creates the interface of the chart

--- src/opennlp/ccg/parse/Chart.java
+++ src/opennlp/ccg/parse/Chart.java
@@ -1,5 +1,26 @@
 package opennlp.ccg.parse;
 
+import java.io.PrintStream;
+import java.util.List;
+
+/**
+ * A chart.
+ * 
+ * WARNING: This is an abstraction of a static data structure. It is not supposed to include methods
+ * for applying grammatical rules nor any other kind of ruled construction. 
+ * 
+ * @author Daniel Couto-Vale
+ */
 public interface Chart {
+
+	List<Cell> getForms();
+
+	List<Cell> getForms(int start);
+
+	List<Cell> getForms(int start, int length);
+
+	List<Cell> makeForm(Edge edge, int start, int length);
+
+	void print(PrintStream out);
 
 }

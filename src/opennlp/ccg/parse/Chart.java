550144876b5f151cf7cbaf2726cf21a98562d632
Daniel Couto Vale <danielvale@uni-bremen.de>
2015-02-02 14:08:34 +0100

    Comment chart

--- src/opennlp/ccg/parse/Chart.java
+++ src/opennlp/ccg/parse/Chart.java
@@ -13,14 +13,44 @@ import java.util.List;
  */
 public interface Chart {
 
+	/**
+	 * Gets all forms
+	 * 
+	 * @return the forms
+	 */
 	List<Cell> getForms();
 
+	/**
+	 * Gets all forms starting at a start position
+	 * 
+	 * @param start the start position
+	 * @return the forms starting at the start position
+	 */
 	List<Cell> getForms(int start);
 
+	/**
+	 * Gets all forms starting at a start position and with a given length
+	 * 
+	 * @param start the start position 
+	 * @param length the length of the forms
+	 * @return the forms starting at the start position and with a given length
+	 */
 	List<Cell> getForms(int start, int length);
 
-	List<Cell> makeForm(Edge edge, int start, int length);
+	/**
+	 * Intantiates a form that realises a grammatical unit
+	 *  
+	 * @param edge the grammatical unit
+	 * @param start the start position of the form
+	 * @param length the length of the form 
+	 */
+	void makeForm(Edge edge, int start, int length);
 
+	/**
+	 * Prints to the output stream
+	 * 
+	 * @param out the output stream
+	 */
 	void print(PrintStream out);
 
 }

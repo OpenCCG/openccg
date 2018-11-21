9b3902c554c392d3369019b21a9796f12037d262
Sebastian Höffner <info@sebastian-hoeffner.de>
2018-11-21 18:21:36 +0100

    xml2ccg: Basic program and test structure

--- src/ccg2xml/test_xml2ccg.py
+++ src/ccg2xml/test_xml2ccg.py
@@ -1,12 +1,199 @@
+import functools
+import sys
 import unittest
 
-import xml2ccg
+from io import StringIO
+from pathlib import Path
+from shutil import rmtree
+from unittest.mock import patch
+
+from ccg2xml import main as ccg2xml
+from xml2ccg import xml2ccg, XMLGrammar
+
+
+TEST_DATA = Path(__file__).parent.parent.parent / 'test' / 'ccg2xml'
+TEMP_DATA = Path(__file__).parent.parent.parent / 'test' / 'ccg2xml' / 'tmp'
+
+
+def xml2ccg_argv(grammar):
+    """Prepares the command line arguments for xml2ccg.
+
+    Args:
+        grammar: The grammar to be used.
+    """
+    args = ['xml2ccg', str(TEST_DATA / grammar)]
+    return patch.object(sys, 'argv', args)
+
+
+def ccg2xml_argv(grammar):
+    """Prepares the command line arguments for ccg2xml.
+
+    Args:
+        grammar: The grammar to be used.
+    """
+    args = ['ccg2xml', '-',
+            '--prefix', '{}-'.format(grammar),
+            '--quiet',
+            '--dir', str(TEMP_DATA / grammar)]
+    return patch.object(sys, 'argv', args)
+
+
+class SysIn:
+    """Wraps sysin so that the input of ccg2xml can be captured easily."""
+    def __enter__(self):
+        self.original_stdin = sys.stdin
+        sys.stdin = StringIO()
+        return sys.stdin
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        sys.stdin = self.original_stdin
+
+
+class SysOut:
+    """Wraps sysout so that the output of xml2ccg can be captured easily."""
+    def __enter__(self):
+        self.original_stdout = sys.stdout
+        sys.stdout = StringIO()
+        return sys.stdout
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        sys.stdout = self.original_stdout
+
+
+def compare_xmls(test_instance, grammar):
+    """Loads the TEST and TEMP grammars specified by grammar using XMLGrammar.
+
+    Each individual part, as specified in XMLGrammar.valid_filenames, is
+    compared and asserted. This is collected as "soft asserts" and reported
+    completely at the end.
+
+    Args:
+        test_instance: Should be a test object, usually "self" when using
+                       unittest.
+        grammar: The grammar to be used.
+
+    Raises:
+        AssertionError if not all parts of the grammars are equal.
+    """
+    original = XMLGrammar(TEST_DATA / grammar)
+    generated = XMLGrammar(TEMP_DATA / grammar)
+
+    test_map = {}
+    for fn in XMLGrammar.valid_filenames:
+        test_map[fn] = {
+            'original': getattr(original, fn),
+            'generated': getattr(generated, fn)
+        }
+        # TODO(shoeffner): ElementTrees are not == comparable
+        test_map[fn]['test'] = test_map[fn]['original'] == test_map[fn]['generated']
+
+    assert all(test_map[k]['test'] for k in test_map.keys()), generate_message(test_map)
+
+
+def generate_message(test_map):
+    """Generates a slightly more informative assertion message in case a test fails.
+
+    Args:
+        test_map: A test map of the form:
+                    {
+                        'test_key': {
+                            'original': the expected value,
+                            'generated': the actual value,
+                            'test': the test result (True or False)
+                        }
+                        ...
+                    }
+    Returns:
+        A string listing each test key with its original and generated values,
+        but only for those where the test value is False.
+    """
+    tmpl = """{fn} test failed:
+    Generated:
+        {generated}
+
+    Original:
+        {original}
+    """
+    messages = []
+    for k, v in test_map.items():
+        if v['test']:
+            continue
+        messages.append(tmpl.format(fn=k, **v))
+
+    return '\n'.join(['The two grammars do not match.'] + messages)
+
+
+def test_grammar(grammar):
+    """Simplifies access to multiple similar test cases.
+
+    A function annotated with test_grammar converts the grammar inside the
+    TEST_DATA directory with xml2ccg into a ccg file and passes it to ccg2xml.
+    ccg2xml in turn outputs the new set of xml files into TEMP_DATA / grammar.
+
+    Then, both xml grammars are loaded and their xml files compared.
+
+    Args:
+        grammar: The grammar to be used.
+    """
+    def wrapper(func):
+        @functools.wraps(func)
+        def test_function(self, *args, **kwargs):
+
+            with xml2ccg_argv(grammar), SysOut() as sout:
+                xml2ccg()
+                generated_ccg = sout.getvalue()
+
+            with ccg2xml_argv(grammar), SysIn() as sin:
+                sin.write(generated_ccg)
+                ccg2xml()
+
+            compare_xmls(self, grammar)
+
+        return test_function
+    return wrapper
 
 
 class TestXML2CCG(unittest.TestCase):
+    """This class tests the xml2ccg functionality in conjunction with ccg2xml.
 
-    def test_xml2ccg_1(self):
-        self.assertEqual(xml2ccg.m1(), 1)
+    While the test methods only use the generated XML files present in
+    subdirectories of ${OPENCCG_HOME}/test/ccg2xml, the doc strings of each
+    test method mention the original source files.
 
-    def test_xml2ccg_2(self):
-        self.assertEqual(xml2ccg.m2(), 1)
+    There are some *.ccg files inside the ${OPENCCG_HOME}/test/ccg2xml
+    directory. Those are specialized test cases and also available as
+    pre-compiled xml grammar directories.
+    """
+
+    @test_grammar('grammar_template')
+    def test_grammar_template(self):
+        """${OPENCCG_HOME}/src/ccg2xml/grammar_template.ccg"""
+        pass
+
+    @test_grammar('tinytiny')
+    def test_tinytiny(self):
+        """${OPENCCG_HOME}/ccg-format-grammars/tinytiny/tinytiny.ccg"""
+        pass
+
+    @test_grammar('tiny')
+    def test_tiny(self):
+        """${OPENCCG_HOME}/ccg-format-grammars/tiny/tiny.ccg"""
+        pass
+
+    @test_grammar('inherit')
+    def test_inherit(self):
+        """${OPENCCG_HOME}/ccg-format-grammars/inherit/inherit.ccg"""
+        pass
+
+    @test_grammar('arabic')
+    def test_arabic(self):
+        """${OPENCCG_HOME}/ccg-format-grammars/arabic/arabic.ccg
+           ${OPENCCG_HOME}/src/ccg2xml/arabic.ccg
+           (Both files are the same)
+        """
+        pass
+
+    @classmethod
+    def tearDownClass(cls):
+        """Removes the temp data after the tests."""
+        rmtree(TEMP_DATA)
--- src/ccg2xml/xml2ccg.py
+++ src/ccg2xml/xml2ccg.py
@@ -1,9 +1,73 @@
 #!/usr/bin/env python3
 
+import argparse
+import sys
+import xml.etree.ElementTree as ET
+from pathlib import Path
 
-def m1():
-    return 1
+
+class XMLGrammar:
+    """This class wraps a single directory containing all xml files for an
+    OpenCCG grammar into a flat API.
+
+    Each file (grammar.xml, morph.xml, ...), as defined in
+    XMLGrammar.valid_filenames, can be accessed as an XML ElementTree as a
+    simple attribute:
+
+        xml_grammar = XMLGrammar(path_to_grammar)
+        xml_grammar.morph  # The parsed (prefix-)morph.xml of the grammar
+        xml_grammar.rules  # The parsed (prefix-)rules.xml of the grammar
+    """
+    valid_filenames = ['grammar', 'morph', 'lexicon', 'rules', 'types', 'testbed']
+
+    def __init__(self, path):
+        self.path = Path(path)
+
+    def __getattribute__(self, attr):
+        if attr in XMLGrammar.valid_filenames:
+            try:
+                return ET.parse(next(self.path.glob('*' + attr + '.xml')))
+            except StopIteration:
+                return ET.ElementTree()
+        return object.__getattribute__(self, attr)
 
 
-def m2():
-    return 2
+def convert_to_ccg(xml_grammar):
+    """Converts an XMLGrammar to a ccg file string.
+
+    Args:
+        xml_grammar: An XMLGrammar representing the grammar xml files.
+
+    Returns:
+        A string which can be stored as a *.ccg file and converted via
+        ccg2xml.
+    """
+    # TODO(shoeffner): Do the actual conversion :-)
+    return ''
+
+
+def xml2ccg():
+    """Entry point of the program."""
+    arguments = parse_args()
+
+    result = convert_to_ccg(arguments.folder)
+
+    print(result, file=arguments.output)
+
+
+def parse_args():
+    """Parses the program aguments.
+
+    Returns:
+        The parsed arguments.
+    """
+    parser = argparse.ArgumentParser()
+    parser.add_argument('folder', nargs='?', type=XMLGrammar, default='.',
+                        help="The path to the directory containing the grammar's xml files.")
+    parser.add_argument('-o', '--output', nargs='?', type=argparse.FileType('w'),
+                        default=sys.stdout, help="The output file. Defaults to STDOUT.")
+    return parser.parse_args()
+
+
+if __name__ == '__main__':
+    xml2ccg()

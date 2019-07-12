92bbf0b0d87ce511dffb0931f0f2af91612ec3ed
Šarūnas Navickas <zaibacu@gmail.com>
2019-07-12 15:51:34 +0300

    Allow to actually build Lexicon

--- src/opennlp/ccg/grammar/builders/LexiconBuilder.java
+++ src/opennlp/ccg/grammar/builders/LexiconBuilder.java
@@ -1,6 +1,7 @@
 package opennlp.ccg.grammar.builders;
 
-import opennlp.ccg.lexicon.Lexicon;
+import opennlp.ccg.lexicon.*;
+import java.util.*;
 
 public class LexiconBuilder {
     public static LexiconBuilder builder(){
@@ -8,11 +9,27 @@ public class LexiconBuilder {
     }
 
     private Lexicon lexicon;
+    private List<Family> familyList = new ArrayList<>();
+    private List<MorphItem> morphList = new ArrayList<>();
+    private List<MacroItem> macroList = new ArrayList<>();
     private LexiconBuilder(){
         this.lexicon = new Lexicon();
     }
 
+    public void addFamily(Family family){
+        this.familyList.add(family);
+    }
+
+    public void addMorph(MorphItem item){
+        this.morphList.add(item);
+    }
+
+    public void addMacro(MacroItem item){
+        this.macroList.add(item);
+    }
+
     public Lexicon build(){
+        this.lexicon.init(familyList, morphList, macroList);
         return this.lexicon;
     }
 }
